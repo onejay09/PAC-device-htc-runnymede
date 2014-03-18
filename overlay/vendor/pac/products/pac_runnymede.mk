525c8fe15f95f1270793d989c54a7e1a5b8aff21
ranked001 <ranked001@BeAsT.(none)>
2014-03-18 12:04:57 +0000

    first commit

---
+++ overlay/vendor/pac/products/pac_runnymede.mk
@@ -0,0 +1,30 @@
+# Check for target product
+ifeq (pac_runnymede,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_runnymede
+
+# PAC device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/pac/common
+
+# PAC boot logo
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/common/bootlogo/pac_logo_480x800.rle:root/GIO.rle
+
+# Copy bootanimation
+PRODUCT_COPY_FILES += \
+    vendor/pac/prebuilt/480x800/bootanimation.zip:system/media/bootanimation.zip
+
+# include PAC common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/htc/runnymede/cm.mk)
+
+PRODUCT_NAME := pac_runnymede
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/pac/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/pac/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
